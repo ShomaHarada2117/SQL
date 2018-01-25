5df36a8cd312f05beb8ef73dac96535b06d2aa64
DESKTOP-S120DCG\likeit_student <harasho2117@gmail.com>
2018-01-25 19:00:14 +0900

    問題全て終了。

--- q1.sql
+++ q1.sql
@@ -26,4 +26,26 @@ SELECT item_name, item_price FROM item WHERE category_id =1;
 SELECT item_name, item_price FROM item WHERE item_price >=1000;
 
 問７．
-SELECT item_name, item_price FROM item WHERE item_name LIKE '肉';
+SELECT item_name, item_price FROM item WHERE item_name LIKE '%肉%';
+
+
+					//SELECT
+					    顧客表.* ,伝票表.*                   (顧客と伝票の情報を抽出したいなー)
+					FROM
+					    顧客表 INNER JOIN 伝票表             (カラム同士を結合)(内部結合)
+					    ON 顧客表.顧客№ = 伝票表.顧客№     (結合規則ONでカラムの値が一致するデータのみ取得)(テーブル名.カラム名)
+					WHERE
+					    顧客表.顧客名 = '鈴木'               (条件付けたち)
+					    AND 伝票表.日付 = '8/20'
+					//    
+
+					SQLの制約たち
+					・主キー制約…被るな、何か入れろ。
+					・一意性制約(UNIQUE)…他の行の値と重複しちゃダメ制約、ユニーク。
+					・NOT NULL制約…カラムに格納する値としてNULLを禁止したい場合、カラムにNOT NULL制約を設定します。
+
+問８．
+SELECT item_id, item_name, item_price, category_name FROM item INNER JOIN item_category ON item.category_id = item_category.category_id;
+
+問９．
+SELECT category_name, SUM(item_price)total_price FROM item INNER JOIN item_category ON item.category_id = item_category.category_id GROUP BY item.category_id ORDER BY total_price DESC;
